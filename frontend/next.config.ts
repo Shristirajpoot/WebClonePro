11b4d3005b2bf2e3780e820d86ca08aec9e4be00
👩‍💻Shristi Rajpoot <147296860+Shristirajpoot@users.noreply.github.com>
2025-06-06 23:41:55 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,  // Helps catch potential bugs in development
-  swcMinify: true,        // Enables faster minification for production builds
+       // Enables faster minification for production builds
 };
 
 export default nextConfig;
